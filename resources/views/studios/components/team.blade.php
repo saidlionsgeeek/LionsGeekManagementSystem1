fdde9a8408f04725c411862a752c79f506ca8584
Ayman Ait Laaguid <laaguid.ayman@gmail.com>
2023-09-10 15:31:01 +0100

    byuser

--- resources/views/studios/components/team.blade.php
+++ resources/views/studios/components/team.blade.php
@@ -12,11 +12,13 @@
                 <td>{{ $team_member->user->name}} {{ $team_member->user->lastname}} </td>
                 <td>{{ $team_member->user->type }}</td>
                 <td >
+                    @if ($reservation_studio->user_id == auth()->user()->id || auth()->user()->name == 'admin')
                     <form action={{ route('reservation_studio_team.delete', $team_member->id) }} method="POST" >
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger">X</button>
                     </form>
+                    @endif
                 </td>
             </tr>
         @endforeach
