46cff414d01dffa2a95561afcaed3c265b3d3d1b
saidlionsgeek <saidlionesgeek.1997@gmail.com>
2023-09-12 22:44:59 +0100

    user boolean

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -37,6 +38,9 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        $user = User::find(auth()->user()->id);
+            $user->emailverification = false;
+            $user->save();
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
